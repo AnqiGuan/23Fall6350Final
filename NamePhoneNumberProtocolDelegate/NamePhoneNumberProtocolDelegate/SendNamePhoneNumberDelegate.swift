828556f7d7964171f23b89f1dd3b443cd9f2040f
Anqi Guan <63136314+AnqiGuan@users.noreply.github.com>
2023-12-09 16:32:02 -0800

    Create the same app as 3 but get the values from a different VC
    Create the same app as 3 but get the values from a different VC, once the name and phone number is added in the Second VC use protocol and delegate to get the values the Name and Phone number put it in the table vc for the first VC.

---
+++ NamePhoneNumberProtocolDelegate/NamePhoneNumberProtocolDelegate/SendNamePhoneNumberDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  SendNamePhoneNumberDelegate.swift
+//  NamePhoneNumberProtocolDelegate
+//
+//  Created by Anqi Guan on 12/9/23.
+//
+
+import Foundation
+protocol SendNamePhoneNumberDelegate{
+    func sendNamePhoneNumber(name: String, phoneNumber: String)
+}
